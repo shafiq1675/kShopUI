547bef3031b37d1a2921a56b2c16c0ef95e101f3
Md. Shafiqul Islam <islam.mdshafiq@gmail.com>
2022-02-01 22:16:13 +0600

    third

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerSetupComponent} from './Customer/customer-setup/customer-setup.component'
 
 const routes: Routes = [
-
+  { path: 'customer-setup', component: CustomerSetupComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerSetupComponent } from './Customer/customer-setup/customer-setup.component';
 import { ProductSetupComponent } from './Product/product-setup/product-setup.component';
 import { ProductOrderComponent } from './Order/product-order/product-order.component';
-import { MatInput, MatInputModule } from '@angular/material/input';
+// import { MatInput, MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
+import { FormsModule} from '@angular/forms';
+// import {MatTableModule} from '@angular/material/table';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatSelectModule} from '@angular/material/select';
+import {MaterialExampleModule} from '../material.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,9 @@ import { MatInput, MatInputModule } from '@angular/material/input';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
