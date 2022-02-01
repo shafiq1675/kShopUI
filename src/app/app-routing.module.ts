cbc12fcfbc7c47a9684bca2f97043597282da4c0
Md. Shafiqul Islam <islam.mdshafiq@gmail.com>
2022-02-01 23:23:26 +0600

    product setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomerSetupComponent} from './Customer/customer-setup/customer-setup.component'
+import { ProductSetupComponent } from './Product/product-setup/product-setup.component';
 
 const routes: Routes = [
   { path: 'customer-setup', component: CustomerSetupComponent },
+  { path: 'product-setup', component: ProductSetupComponent },
 ];
 
 
